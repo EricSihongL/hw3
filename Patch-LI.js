6fd7b79f08509d7303db9ebd34e26933b50c63ad
EricSihongL <31781716+EricSihongL@users.noreply.github.com>
2017-10-04 01:36:44 -0700

    Create Patch-LI.js

---
+++ Patch-LI.js
@@ -0,0 +1,10 @@
+fill(100,100,200);
+rectMode(CENTER);
+rect(width/3.5, height/2,100,100);
+line(width/5.5, height/1.8, width/3, height/1.8);
+line(width/5.5,height/2.5, width/5.5,height/1.8);
+
+fill(225,100,200);
+rectMode(CENTER);
+rect(width/1.5, height/2,100,100);
+line(width/1.5,height/1.8,width/1.5, height/2.5);
